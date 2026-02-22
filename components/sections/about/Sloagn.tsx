4c9996830081b656fa5822000fcfa05262b65154
bassemMohamed-h <engbesoisa@gmail.com>
2026-02-22 10:36:16 +0200

    small edit on sloagn

--- components/sections/about/Sloagn.tsx
+++ components/sections/about/Sloagn.tsx
@@ -1,8 +1,8 @@
 export function Slogan() {
     return (
-        <section className="Slogan min-h-[100svh] bg-third text-primary flex items-center text-center ">
+        <section className="Slogan min-h-[100svh] bg-third text-primary flex items-center justify-center ">
             <div>
-                <h2 className="text-4xl font-bold tracking-widest mb-20 ">
+                <h2 className="text-5xl font-bold tracking-widest mb-20 ">
                    Design is not decoration It is a way of thinking
                 </h2>
                 <p className=" text-xl leading-relaxed text-black w-[50%] mx-auto">
